1843b799621e3cf3480c2961d78121c1f422180d
Roland <kalman.roland@gmail.com>
2021-04-17 10:26:17 +0200

    added Address Controller

---
+++ PizzaJo API/Controllers/AddressController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MySql.Data.MySqlClient;
+using pizzajo_api;
+
+namespace PizzaJo_API.Controllers
+{
+    public class AddressController : ApiController
+    {
+        // this class is used when all orders are fetched
+        public class all_address
+        {
+            public int house_number { get; set; }
+            public string street_name { get; set; }
+            public string city_name { get; set; }
+            public string zipcode { get; set; }
+            public string error { get; set; }
+
+            public all_address(int house_number, string street_name, string city_name, string zipcode, string error)
+            {
+                this.house_number = house_number;
+                this.street_name = street_name;
+                this.city_name = city_name;
+                this.zipcode = zipcode;
+                this.error = error;
+            }
+        }
+
+        // GET: fetch all food (by category)
+        public List<all_address> Get()
+        {
+            MySqlConnection conn = WebApiConfig.conn();
+
+            MySqlCommand query = conn.CreateCommand();
+
+            query.CommandText = "CALL `getAddress`();";
+           
+            var result = new List<all_address>();
+
+            try
+            {
+                conn.Open();
+                MySqlDataReader fetch_query = query.ExecuteReader();
+
+                while (fetch_query.Read())
+                {
+
+                    result.Add(new all_address(Convert.ToInt32(fetch_query["house_number"]), fetch_query["street_name"].ToString(), fetch_query["city_name"].ToString(), fetch_query["zipcode"].ToString(), ""));
+                }
+            }
+            catch (MySqlException ex)
+            {
+                result.Add(new all_address(0, null, null, null, ex.ToString()));
+            }
+
+            conn.Close();
+
+            return result;
+        }
+    }
+}
--- PizzaJo API/Controllers/FoodController.cs
+++ PizzaJo API/Controllers/FoodController.cs
@@ -12,39 +12,46 @@ namespace pizzajo_api.Controllers
     public class FoodController : ApiController
     {
         // this class is used when all orders are fetched
-        public class all_orders
+        public class all_food
         {
-            public int order_id { get; set; }
-            public int customer_customer_id { get; set; }
-            public int payment_payment_id { get; set; }
-            public string order_time { get; set; }
-            public string order_note { get; set; }
-            public char order_status { get; set; }
+            public string food_name { get; set; }
+            public int food_price { get; set; }
+            public string food_ingredients { get; set; }
+            public byte[] food_picture { get; set; }
             public string error { get; set; }
 
-            public all_orders(int order_id, int customer_customer_id, int payment_payment_id, string order_time,
-                string order_note, char order_status, string error)
+            public all_food(string food_name, int food_price, string food_ingredients, byte[] food_picture, string error)
             {
-                this.order_id = order_id;
-                this.customer_customer_id = customer_customer_id;
-                this.payment_payment_id = payment_payment_id;
-                this.order_time = order_time;
-                this.order_note = order_note;
-                this.order_status = order_status;
+                this.food_name = food_name;
+                this.food_price = food_price;
+                this.food_ingredients = food_ingredients;
+                this.food_picture = food_picture;
                 this.error = error;
             }
         }
 
-        // GET: fetch all orders
-        public List<all_orders> Get()
+        // GET: fetch all food (by category)
+        public List<all_food> Get(int food_group_id)
         {
             MySqlConnection conn = WebApiConfig.conn();
 
             MySqlCommand query = conn.CreateCommand();
 
-            query.CommandText = "CALL `select_all_order`();";
+            if (food_group_id == 1) //pizza 
+            {
+                query.CommandText = "CALL `select_all_pizza`();";
+            }
+            if (food_group_id == 2) //hamburger 
+            {
+                query.CommandText = "CALL `select_all_hamburger`();";
+            }
+            if (food_group_id == 3) //gyros 
+            {
+                query.CommandText = "CALL `select_all_gyros`();";
+            }
 
-            var result = new List<all_orders>();
+
+            var result = new List<all_food>();
 
 
             try
@@ -54,18 +61,19 @@ namespace pizzajo_api.Controllers
 
                 while (fetch_query.Read())
                 {
-                    result.Add(new all_orders(Convert.ToInt32(fetch_query["order_id"]), Convert.ToInt32(fetch_query["customer_customer_id"]), Convert.ToInt32(fetch_query["payment_payment_id"]), fetch_query["order_time"].ToString(), fetch_query["order_note"].ToString(), Convert.ToChar(fetch_query["order_status"]), ""));
+                    byte[] picture = (byte[])fetch_query.GetValue(3); //4th column
+
+                    result.Add(new all_food(fetch_query["food_name"].ToString(), Convert.ToInt32(fetch_query["food_price"]), fetch_query["food_ingredients"].ToString(), picture, ""));
                 }
             }
             catch (MySqlException ex)
             {
-                result.Add(new all_orders(0, 0, 0, "", "", ' ', ex.ToString()));
+                result.Add(new all_food(null, 0, null, null, ex.ToString()));
             }
 
             conn.Close();
 
             return result;
         }
-
     }
 }
